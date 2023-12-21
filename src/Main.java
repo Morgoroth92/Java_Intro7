4010b296f46204e5f153fc6811c906522f63be4a
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2023-12-21 12:05:58 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+      divisione(12, 3);
+
+    }
+    public static void divisione(int a, int b) {
+     float z = (float)a / (float) b;
+        System.out.println("il risultato della dividisione è :" + z);
+    }
+}
